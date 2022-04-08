784dd80395ae98a75b48ca28e84c71797dfec298
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-04-08 12:27:24 -0500

    Se agrego campo timestamp a la colección posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,143 +1,139 @@
 // eslint-disable-next-line import/no-cycle
-import { onNavigate } from '../main.js';
-// eslint-disable-next-line import/no-cycle
-import {
-   logOut
- } from '../firebase/auth.js';
+import { logOut } from "../firebase/auth.js";
 
-import { 
-  publication, 
-  getPublication, 
+import {
+  publication,
+  getPublication,
   onGetPublication,
   deletePublication,
   getPost,
   updatePublication,
-  likes,
- } from '../firebase/firestore.js';
+  // likes,
+} from "../firebase/firestore.js";
 
-import { 
-  postsTemplate
- } from './template.js';
+import { postsTemplate } from "./template.js";
 
 export const home = () => {
-  const loginDiv = document.createElement('div');
-  loginDiv.setAttribute('class', 'loginDiv');
-  const profileDiv = document.createElement('div');
-  profileDiv.setAttribute('class', 'profileDiv');
-  const imgProfileDiv = document.createElement('IMG');
-  imgProfileDiv.setAttribute('src', './Image/fondoCiudad.PNG');
-  imgProfileDiv.setAttribute('class', 'imgProfileDiv');
-  const imgProfile = document.createElement('IMG');
-  imgProfile.setAttribute('src', './Image/22Perfil.png');
-  imgProfile.setAttribute('class', 'imgProfile');
-  const nameDiv = document.createElement('div');
-  nameDiv.setAttribute('class', 'nameDiv');
-  const btnLogOut = document.createElement('button');
-  btnLogOut.setAttribute('class', 'btnLogOut');
-  const postDiv = document.createElement('div');
-  const formPublication = document.createElement('form');
-  formPublication.setAttribute('class', 'formPublication');
-  const publicationTitle =  document.createElement('input');
-  publicationTitle.setAttribute('placeholder', '¿Qué quieres compartir?');
-  publicationTitle.setAttribute('class', 'publicationTitle');
-  const publicationText =  document.createElement('textarea');
-  publicationText.setAttribute('placeholder', 'Escribe aquí');
-  publicationText.setAttribute('class', 'publicationText');
-  publicationText.setAttribute('rows', '5');
-  const btnSave = document.createElement('button');
-  btnSave.setAttribute('class', 'btnSave');
-  btnSave.setAttribute('id', 'btnSave');
-  const containerPublication = document.createElement('div');
+  const loginDiv = document.createElement("div");
+  loginDiv.setAttribute("class", "loginDiv");
+  const profileDiv = document.createElement("div");
+  profileDiv.setAttribute("class", "profileDiv");
+  const imgProfileDiv = document.createElement("IMG");
+  imgProfileDiv.setAttribute("src", "./Image/fondoCiudad.PNG");
+  imgProfileDiv.setAttribute("class", "imgProfileDiv");
+  const imgProfile = document.createElement("IMG");
+  imgProfile.setAttribute("src", "./Image/22Perfil.png");
+  imgProfile.setAttribute("class", "imgProfile");
+  const nameDiv = document.createElement("div");
+  nameDiv.setAttribute("class", "nameDiv");
+  const btnLogOut = document.createElement("button");
+  btnLogOut.setAttribute("class", "btnLogOut");
+  const postDiv = document.createElement("div");
+  const formPublication = document.createElement("form");
+  formPublication.setAttribute("class", "formPublication");
+  const publicationTitle = document.createElement("input");
+  publicationTitle.setAttribute("placeholder", "¿Qué quieres compartir?");
+  publicationTitle.setAttribute("class", "publicationTitle");
+  const publicationText = document.createElement("textarea");
+  publicationText.setAttribute("placeholder", "Escribe aquí");
+  publicationText.setAttribute("class", "publicationText");
+  publicationText.setAttribute("rows", "5");
+  const btnSave = document.createElement("button");
+  btnSave.setAttribute("class", "btnSave");
+  btnSave.setAttribute("id", "btnSave");
+  const containerPublication = document.createElement("div");
 
-  btnLogOut.textContent = 'Cerrar Sesión';
-  btnSave.textContent = 'Publicar';
+  btnLogOut.textContent = "Cerrar Sesión";
+  btnSave.textContent = "Publicar";
 
-  btnLogOut.addEventListener('click', () => {
+  btnLogOut.addEventListener("click", () => {
     logOut();
   });
 
-  nameDiv.textContent = localStorage.getItem('email');
+  nameDiv.textContent = localStorage.getItem("email");
 
   let editStatus = false;
-  let id = '';
-  
-  formPublication.addEventListener('submit', (e) => {
-    e.preventDefault()
-    if(!editStatus){
-      publication(publicationTitle.value, publicationText.value, localStorage.getItem('usuario')); 
-    } else { 
-      updatePublication( id,{ 
-        title: publicationTitle.value, 
-        text: publicationText.value, 
-        user: localStorage.getItem('usuario'),
+  let id = "";
+ 
+  let getLikes = [localStorage.getItem("usuario")];
+  // let time = updateTimestamp;
+
+  formPublication.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!editStatus) {
+      publication(
+        publicationTitle.value,
+        publicationText.value,
+        localStorage.getItem("usuario"),
+        // getLikes,
+      );
+    } else {
+      updatePublication(id, {
+        title: publicationTitle.value,
+        text: publicationText.value,
       });
       editStatus = false;
     }
     formPublication.reset();
-});
+  });
 
-onGetPublication((querySnapshot) =>{
-getPublication()
-.then((data) => { 
- postsTemplate(data, containerPublication);
+  onGetPublication((querySnapshot) => {
+    getPublication()
+      .then((data) => {
+        postsTemplate(data, containerPublication);
 
- const btnsDelete = containerPublication.querySelectorAll('.btnsDelete');
- console.log(btnsDelete);
- btnsDelete.forEach( btn => {
-   btn.addEventListener('click', ({target: {dataset}}) => {
-    deletePublication(dataset.id)
-   })
- })
+        const btnsDelete = containerPublication.querySelectorAll(".btnsDelete");
+        console.log(btnsDelete);
+        btnsDelete.forEach((btn) => {
+          btn.addEventListener("click", ({ target: { dataset } }) => {
+            deletePublication(dataset.id);
+          });
+        });
 
+        const btnsEdit = containerPublication.querySelectorAll(".btnsEdit");
+        console.log(btnsEdit);
+        btnsEdit.forEach((btn) => {
+          btn.addEventListener("click", async (e) => {
+            const doc = await getPost(e.target.dataset.id);
+            const publication = doc.data();
 
- const btnsEdit = containerPublication.querySelectorAll('.btnsEdit');
- console.log(btnsEdit);
- btnsEdit.forEach((btn) => {
-   btn.addEventListener('click', async (e) => {
-    const doc = await getPost(e.target.dataset.id);
-    const publication = doc.data();
+            publicationTitle.value = publication.title;
+            publicationText.value = publication.text;
 
-    publicationTitle.value = publication.title;
-    publicationText.value = publication.text;
+            editStatus = true;
+            id = e.target.dataset.id;
+            btnSave.textContent = "Actualizar";
+          });
+        });
 
-    editStatus = true;
-    id = e.target.dataset.id;
-    btnSave.textContent = 'Actualizar';
+        // const btnsLikes = containerPublication.querySelectorAll(".btnsLikes");
+        // const count = containerPublication.querySelectorAll("#count");
+        // console.log(btnsLikes);
 
-  })
-})
+        // getLikes()
+        //   .then(() => {
+        //     const user = localStorage.getItem("usuario");
+        //     btnsLikes.forEach((btn) => {
+        //       btn.addEventListener("click", () => {
+        //         if (!data.includes(user) ) {
+        //           const likeUser = likes(user);
+        //           console.log(likeUser);
+        //         } else {
+                  
+        //         }
+        //         count.textContent = data.length;
+        //       });
+        //     });
+        //   })
+        //   .catch((err) => {
+        //     console.log(err);
+        //   });
 
-const btnsLikes = containerPublication.querySelectorAll('.btnsLikes');
-const count = containerPublication.querySelectorAll('#count');
-console.log(btnsLikes);
-
-const user = localStorage.getItem('usuario');
-btnsLikes.forEach((btn) => {
-  btn.addEventListener('click',  () => {
-    if(data.includes(user) !== true){
-    const likeUser = likes(user);  
-    console.log(likeUser);
-    getLikes()
-    .then (( ) => { 
-      count.textContent = data.length;
-    })
-    .catch ((err) =>{ 
-      console.log(err);
-    })
-    } else if (data.includes(user) !== false){ 
-    
-      count.textContent =  data.length;
-    }
-  })
-})
-
-
-})
-.catch((err) => {
-  console.log(err);
-});
-});
-
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
   loginDiv.appendChild(profileDiv);
   profileDiv.appendChild(imgProfileDiv);
@@ -151,6 +147,5 @@ btnsLikes.forEach((btn) => {
   loginDiv.appendChild(postDiv);
   loginDiv.appendChild(btnLogOut);
 
- 
   return loginDiv;
-}
+};
--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -8,13 +8,23 @@ import {
   doc,
   getDoc,
   updateDoc,
+  serverTimestamp,
+  // orderBy,
 } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
 
 const db = getFirestore();
+ 
+// const likes =  async (id) => updateDoc(doc(db, "posts", id), { likesPost: arrayUnion(id),
+// });
+// export const updateTimestamp = await updateDoc(docRef, {
+//   timestamp: serverTimestamp()
+// });
 
 export const publication =  async (title, text, user) => {
-return await addDoc(collection(db, 'posts'), { title, text, user, likes});
+return await addDoc(collection(db, 'posts'), { title, text, user,  timestamp: serverTimestamp()});
 }
+
+// orderBy('title' 'desc');
 
 export const getPublication = async () => {
   let  postsCollection = [];
@@ -32,25 +42,19 @@ export const onGetPublication = (callback) => onSnapshot(collection(db, 'posts')
 
 export const deletePublication = (id) => deleteDoc(doc(db, 'posts', id));
 
-export const getPost = (id)=> getDoc(doc(db, 'posts', id));
+export const getPost = (id) => getDoc(doc(db, 'posts', id));
 
 export const updatePublication = (id, newField) => updateDoc(doc(db, 'posts', id), newField);
 
+//crear funcion para obtener el tiempo 
 
-// export const likes =  async (user) => {
-//   return await addDoc(collection(db, 'likes'), { user});
-//   }
+// //crear funcion para obtener los likes
+// export const onGetLikes = (callback) => onSnapshot(collection(db, 'likes'), callback);
+// const inLikes = async (id, idUserLike) => updateDoc(doc(db, "posts", id), {
+//   likesPost: arrayUnion(idUserLike),
+// });
 
-//   export const getLikes = async () => {
-//     let  likesCollection = [];
-//     const querySnapshot = await getDocs(collection(db, 'likes'));
-//     querySnapshot.forEach((doc) => {
-//      likesCollection.push(doc);
-//      console.log(doc);
-//      console.log(doc.data());
-//     });
-//     console.log(likesCollection);
-//       return  likesCollection
-//   }
-
-//   export const onGetLikes = (callback) => onSnapshot(collection(db, 'likes'), callback);
+// //crear funcion para remover 
+// const desLikes = async (idPost, idUserLike) => updateDoc(doc(db, "posts", idPost), {
+//   likesPost: arrayRemove(idUserLike),
+// });
