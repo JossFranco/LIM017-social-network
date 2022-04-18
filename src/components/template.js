7dfaf0a4f73756d166a1aa72590da6e6578950e9
JossFranco <jossfranco70@gmail.com>
2022-04-18 17:47:08 -0500

    Grid

--- src/components/template.js
+++ src/components/template.js
@@ -1,7 +1,9 @@
-export function postsTemplate (doc, place) {
-  place.innerHTML= '';
+export function postsTemplate(doc, place) {
+  // eslint-disable-next-line no-param-reassign
+  place.innerHTML = '';
   doc.forEach((docPosts) => {
-  place.innerHTML += `
+    // eslint-disable-next-line no-param-reassign
+    place.innerHTML += `
   <div class = "containerPublicationDiv" >
     <div class = "containerPosts">
       <div class = "containerDate">
@@ -12,8 +14,8 @@ export function postsTemplate (doc, place) {
       <p class = "descriptionPosts"> ${docPosts.data().text}</p>
       <p class = "authorPublication" id = "author"> <img src="./Image/imgPerfil.png" class = "imgAuthor" alt="user"> ${docPosts.data().author} </p> 
       <div class = "containerBtns" id ="containerBtns">
-      <button class = "btnsDelete btnsStyle" data-id = "${docPosts.id}" ${docPosts.data().author===localStorage.getItem('email')? '':'disabled'} ><i class="fa-regular fa-trash-can"></i></button>
-      <button class = "btnsEdit btnsStyle" data-id ="${docPosts.id}" ${docPosts.data().author===localStorage.getItem('email')? '':'disabled'}> <i class="fa-regular fa-pen-to-square"></i> </i></button>
+      <button class = "btnsDelete btnsStyle" data-id = "${docPosts.id}" ${docPosts.data().author === localStorage.getItem('email') ? '' : 'disabled'} ><i class="fa-regular fa-trash-can"></i></button>
+      <button class = "btnsEdit btnsStyle" data-id ="${docPosts.id}" ${docPosts.data().author === localStorage.getItem('email') ? '' : 'disabled'}> <i class="fa-regular fa-pen-to-square"></i> </i></button>
       <button class = "btnsLikes btnsStyle btnLikeBlue" data-id ="${docPosts.id}"} > 
       <span id = "iconLike" class = "iconLike" ><i class="fa-regular fa-thumbs-up"></i></span><span id = "count" >  ${docPosts.data().likes.length}</span></button>
       </div>
--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import { register } from './components/Register.js';
 import { home } from './components/Home.js';
 
 const rootDiv = document.getElementById('root');
-
 const routes = {
   '/': login,
   '/register': register,
@@ -15,72 +14,13 @@ const routes = {
 };
 export const onNavigate = (pathname) => {
   window.history.pushState({}, pathname, window.location.origin + pathname);
-
   while (rootDiv.firstChild) {
     rootDiv.removeChild(rootDiv.firstChild);
   }
-
   rootDiv.appendChild(routes[pathname]());
 };
-
 const component = routes[window.location.pathname];
 window.onpopstate = () => {
   rootDiv.appendChild(component());
 };
 rootDiv.appendChild(component());
-
-// if(typeof(Storage) !== 'undefined'){
-  //   //Saludar
-  //   saludar();
-  //   //Contador
-  //   contador();
-  //  //Botones de incrementar y decrementar
-  //  document.getElementById('incrementar').addEventListener('click', incrementar);
-  //  document.getElementById('decrementar').addEventListener('click', decrementar);
-  //  document.getElementById('logOut').addEventListener('click', logOut);
-  // } else {
-  //   alert('Elnavegador NO soporta WebStorage');
-  // }
-  
-  
-  // function saludar(){
-  //   // Saludo al usuario
-  //   alert('El navegador soporta WebStorage');
-  //     if(localStorage.getItem('usuario') != null){
-  //       document.getElementById('saludo').innerHTML = 'Bienvenido/a de nuevo,  '
-  //        +localStorage.getItem('usuario') + '!';
-  //     }else{
-  //       var nombre = prompt('¿Cómo te llamas?')
-  //       localStorage.setItem('usuarios', nombre);
-  //       document.getElementById('saludo').innerHTML = 'Tu primera visita ' +localStorage.getItem('usuario');
-  //     }
-  //   }
-  
-  // function contador(){
-  //   //Contador
-  //   if(!sessionStorage.getItem('contador')){
-  //     sessionStorage.setItem('contador', '0')
-  //   }
-  //     document.getElementById('contador').innerHTML = 'Contador; '+ sessionStorage.getItem('contador');
-   
-  // }
-  
-  // //incrementar
-  
-  // function incrementar () {
-  //  sessionStorage.setItem('contador', Number(sessionStorage.getItem('contador'))+1);
-  //  document.getElementById('contador').innerHTML ) 'Contador: '+sessionStorage.getItem('contador');
-  
-  // }
-  
-  
-  // function incrementar () {
-  //   sessionStorage.setItem('contador', Number(sessionStorage.getItem('contador')) -1);
-  //   document.getElementById('contador').innerHTML ) 'Contador: '+sessionStorage.getItem('contador');
-   
-  //  }
-  
-  //  function logOut(){
-  //    alert('Se ha cerrado la sesión de  ' + localStorage.getItem('usuario'));
-  //    localStorage.removeItem('usuario');
-  //    document.getElementById('saludo').innerHTML= '';
