93f7c0adf6fc9dac8337748621af6c8b5278580f
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-04-20 20:52:40 -0500

    Cambios en template.js

--- src/components/template.js
+++ src/components/template.js
@@ -1,7 +1,7 @@
-export function postsTemplate (doc, place) {
-  place.innerHTML= '';
+export function postsTemplate(doc, place) {
+  place.innerHTML = '';
   doc.forEach((docPosts) => {
-  place.innerHTML += `
+    place.innerHTML += `
   <div class = "containerPublicationDiv" >
     <div class = "containerPosts">
       <div class = "containerDate">
@@ -12,12 +12,12 @@ export function postsTemplate (doc, place) {
       <p class = "descriptionPosts"> ${docPosts.data().text}</p>
       <p class = "authorPublication" id = "author"> <img src="./Image/imgPerfil.png" class = "imgAuthor" alt="user"> ${docPosts.data().author} </p> 
       <div class = "containerBtns" id ="containerBtns">
-      <button class = "btnsDelete btnsStyle" data-id = "${docPosts.id}" ${docPosts.data().author===localStorage.getItem('email')? '':'disabled'} ><i class="fa-regular fa-trash-can"></i></button>
-      <button class = "btnsEdit btnsStyle" data-id ="${docPosts.id}" ${docPosts.data().author===localStorage.getItem('email')? '':'disabled'}> <i class="fa-regular fa-pen-to-square"></i> </i></button>
+      <button class = "btnsDelete btnsStyle" data-id = "${docPosts.id}" ${docPosts.data().author === localStorage.getItem('email') ? '' : 'disabled'} ><i class="fa-regular fa-trash-can"></i></button>
+      <button class = "btnsEdit btnsStyle" data-id ="${docPosts.id}" ${docPosts.data().author === localStorage.getItem('email') ? '' : 'disabled'}> <i class="fa-regular fa-pen-to-square"></i> </i></button>
       <button class = "btnsLikes btnsStyle" data-id ="${docPosts.id}"} > 
       <span id = "iconLike" class = "iconLike" ><i class="fa-regular fa-thumbs-up"></i></span><span id = "count" >  ${docPosts.data().likes.length}</span></button>
       </div>
      </div>
   </div>`;
-   })
-};
+  });
+}
